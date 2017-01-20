1e8d7869a49c046fe237f61fe5fb18af5aba6ce9
Bob Verheij <bvcady@wlan-145-94-187-14.wlan.tudelft.nl>
2017-01-20 11:04:45 +0100

    Initial Commit

---
+++ Xcode 8 Git Example/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode 8 Git Example
+//
+//  Created by Bob Verheij on 20/01/2017.
+//  Copyright © 2017 Bob Verheij. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode 8 Git Example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode 8 Git Example
+//
+//  Created by Bob Verheij on 20/01/2017.
+//  Copyright © 2017 Bob Verheij. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
